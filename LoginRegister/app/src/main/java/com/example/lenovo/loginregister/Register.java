1f5fa8af1d98f5edcd25e63d435739478d40eb28
rangga99 <rdrangga95@gmail.com>
2015-10-23 17:36:40 +0700

    Commit Pertama

---
+++ LoginRegister/app/src/main/java/com/example/lenovo/loginregister/Register.java
@@ -0,0 +1,40 @@
+package com.example.lenovo.loginregister;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Register extends AppCompatActivity implements View.OnClickListener {
+
+    Button bRegister;
+    EditText etEmail, etName, etPhone, etPassword, etConfirmPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etName = (EditText) findViewById(R.id.etName);
+        etPhone = (EditText) findViewById(R.id.etPhone);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        etConfirmPassword = (EditText) findViewById(R.id.etConfirmPassword);
+        bRegister = (Button) findViewById(R.id.bRegister);
+
+        bRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.bRegister:
+
+                break;
+        }
+    }
+}
---
+++ LoginRegister/app/src/main/java/com/example/lenovo/loginregister/login.java
@@ -0,0 +1,48 @@
+package com.example.lenovo.loginregister;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class login extends AppCompatActivity implements View.OnClickListener {
+
+    Button bLogin;
+    EditText etEmail, etPassword;
+    TextView tvRegisterLink;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        bLogin = (Button) findViewById(R.id.bLogin);
+        tvRegisterLink = (TextView) findViewById(R.id.tvRegisterLink);
+
+        bLogin.setOnClickListener(this);
+        tvRegisterLink.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.bLogin:
+
+
+                break;
+
+            case R.id.tvRegisterLink:
+                startActivity(new Intent(this, Register.class));
+                break;
+        }
+    }
+}
